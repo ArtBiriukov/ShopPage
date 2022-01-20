e3f61d3f4de2bc465a0918e49b9fbf546851e42c
Artem Biryukov <carbon0093@gmail.com>
2022-01-20 12:46:36 +0300

    feature change main photo

---
+++ main.js
@@ -0,0 +1,25 @@
+/*Смена фотографий */
+
+const productSlides = document.querySelectorAll('.product-cart__slider-slide'),
+  productImg = document.querySelector('.product-cart__img');
+
+for (let slide of productSlides) {
+  slide.addEventListener('click', (e) => {
+
+    const target = e.target;
+    const srcImg = target.previousElementSibling.src;
+
+    productImg.src = srcImg;
+
+    if (slide.classList.contains('active-slide')) {
+      slide.classList.remove('active-slide');
+    } else {
+
+      productSlides.forEach(element => {
+        element.classList.remove('active-slide')
+      });
+
+      slide.classList.add('active-slide');
+    }
+  })
+}
