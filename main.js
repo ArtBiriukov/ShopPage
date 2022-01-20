1fed2296955c11ba103fa5928fa29c1a78581a96
Artem Biryukov <carbon0093@gmail.com>
2022-01-20 17:53:01 +0300

    feature: notific

--- main.js
+++ main.js
@@ -26,9 +26,9 @@ for (let slide of productSlides) {
 
 /*count products*/
 const countWrapper = document.querySelector('.order__count-wrapper'),
-countInput = document.querySelector('.order__count-input');
+  countInput = document.querySelector('.order__count-input');
 
-countWrapper.addEventListener('click', (e)=> {
+countWrapper.addEventListener('click', (e) => {
   const target = e.target;
 
   if (target.classList.contains('order__count-plus')) {
@@ -37,8 +37,8 @@ countWrapper.addEventListener('click', (e)=> {
     if (countInput.value > 5) {
       countInput.value = 5;
     }
-  } 
-  
+  }
+
   if (target.classList.contains('order__count-mines')) {
     countInput.value--;
 
@@ -56,7 +56,7 @@ const openSelect = (btn) => {
   btn.classList.toggle('open-select')
 }
 
-selectWrapper.addEventListener('click', (e)=> {
+selectWrapper.addEventListener('click', (e) => {
   const target = e.target;
 
   if (target.classList.contains('parameters__select')) {
@@ -69,9 +69,7 @@ selectWrapper.addEventListener('click', (e)=> {
     sizeSelectBtn.innerText = text;
     openSelect(sizeSelectBtn);
   }
-
 })
-
 
 /*mobile menu*/
 
@@ -81,7 +79,7 @@ const showMenu = () => {
   navMobile.classList.toggle('show-menu');
 };
 
-document.addEventListener('click', (e)=> {
+document.addEventListener('click', (e) => {
   let target = e.target;
 
   if (target.closest('.mobile__menu') || target.closest('.nav__mobile-close')) {
@@ -89,9 +87,64 @@ document.addEventListener('click', (e)=> {
     showMenu();
   } else {
 
-      if (!target.closest('.nav__mobile') && navMobile.classList.contains('show-menu')) {
-        showMenu();
-      }
+    if (!target.closest('.nav__mobile') && navMobile.classList.contains('show-menu')) {
+      showMenu();
+    }
   }
 
 })
+
+/*notifications*/
+
+const orderBuyWrapper = document.querySelector('.order__buy'),
+  productTitle = document.querySelector('.product-cart__title').innerText;
+
+orderBuyWrapper.addEventListener('click', (e) => {
+  const target = e.target,
+    messageEl = document.querySelector('.notification__message');
+
+  if (target.closest('.order__btn-basket')) {
+
+    if (!messageEl) {
+      createNotification('basket', countInput.value, productTitle);
+    }
+    return;
+  }
+
+  if (target.closest('.order__btn-favorite')) {
+
+    target.closest('.order__btn-favorite').classList.toggle('add__favorite');
+
+    if (!messageEl) {
+      createNotification('favorite', countInput.value, productTitle);
+    }
+    return;
+  }
+})
+
+const createNotification = (btn, count, productName) => {
+  const message = document.createElement('div');
+  message.className = 'notification__message';
+
+  if (btn === 'basket') {
+    message.innerHTML = `Товар <b>${productName}</b> в количестве ${count} единиц добавлен в корзину`;
+  }
+
+  if (btn === 'favorite') {
+    message.innerHTML = `Товар <b>${productName}</b> добавлен в избранное`;
+  }
+
+  document.body.appendChild(message);
+
+  const hightMes = message.clientHeight;
+  message.style.bottom += `${hightMes - 10}px`;
+
+  setTimeout(() => {
+    message.style.bottom = '-100px';
+
+    setTimeout(() => {
+      message.remove();
+    }, 300);
+
+  }, 3000);
+}
